44afc69f660699690c7028d0095493e6d386e208
ProsperLee <lcsshengsss@outlook.com>
2018-09-23 20:22:22 +0800

    1.0

--- application/config/login.php
+++ application/config/login.php
@@ -2,6 +2,8 @@
 
 require "mysqllink.php";
 
+// 1登陆 2注册 3查询用户
+
 //做个路由 action为url中的参数
 @$action = $_POST['action'];
 
@@ -11,6 +13,9 @@ switch ($action) {
         break;
     case '2':
         register();
+        break;
+    case '3':
+        selectusers();
         break;
 }
 
@@ -22,9 +27,9 @@ function login()
     // username usrpwd 字段
     @$nickname = $_POST['username'];
     @$password = $_POST['userpwd'];
-    //echo $nickname . $password;
+    // echo $nickname . $password;
 
-    $sql = "SELECT name,password FROM pl_userslist WHERE name = '" . $nickname . "' AND password = '" . $password . "'";
+    $sql = "SELECT nickname,password FROM pl_userslist WHERE nickname = '" . $nickname . "' AND password = '" . $password . "'";
 
     if (mysqli_num_rows($link->query($sql))) {
         $code = "000200";
@@ -54,15 +59,19 @@ function register()
     @$email = $_POST['useremail'];
     @$phone = $_POST['userphone'];
 
-    $sql = "SELECT name FROM pl_userslist WHERE name = '" . $nickname . "'";
+    $sql = "SELECT nickname FROM pl_userslist WHERE nickname = '" . $nickname . "'";
     if (mysqli_num_rows($link->query($sql))) {
         $code = "000301";
         $message = "用户名已存在";
         $data = "";
         echo json($code, $message, $data);
     } else {
+        // 创建用户Id
+        $iProsperId = substr(str_shuffle("1234567890"),0,8);
+        // 创建时间
+        $createtime = date('Y-m-d h:i:s',time());
         // 插入语句
-        $sql = "INSERT INTO pl_userslist (name, password, email, phone) VALUES ('" . $nickname . "', '" . $password . "', '" . $email . "','" . $phone . "')";
+        $sql = "INSERT INTO pl_userslist (iProsperId, nickname, password, email, phone, createtime) VALUES ('" . $iProsperId . "','" . $nickname . "', '" . $password . "', '" . $email . "','" . $phone . "','" . $createtime . "')";
         // var_dump($link->query($sql));
         if ($link->query($sql) === true) {
             $code = "000200";
@@ -81,4 +90,19 @@ function register()
     $link->close();
 }
 
-
+// 查询用户
+function selectusers()
+{
+    $link = new mysqli(SERVERNAME, USERNAME, PASSWORD, "users");
+    mysqli_query($link, "SET NAMES utf8");
+    $sql = "SELECT * FROM pl_userslist";
+    $result = $link->query($sql);
+    $userslist = [];
+    foreach ($result as $value){
+        array_push($userslist,$value);   
+    }
+    $code = "000200";
+    $message = "查询用户成功";
+    $data = $userslist;
+    echo json($code, $message, $data);
+}
--- application/config/mysqllink.php
+++ application/config/mysqllink.php
@@ -19,7 +19,7 @@ const PASSWORD = "";
 $link = new mysqli(SERVERNAME, USERNAME, PASSWORD);
 
 // 防止中文乱码
-//$link->query("SET NAMES utf8");
+// $link->query("SET NAMES utf8");
 mysqli_query($link, "SET NAMES utf8");
 
 // 检测连接
@@ -27,41 +27,6 @@ if ($link->connect_error) {
     return "连接失败";
 } else {
     return "连接成功";
-}
-
-// 查询数据库是否已经存在
-if (mysqli_select_db($link, 'users')) {
-    return "数据库已存在";
-    $link = new mysqli(SERVERNAME, USERNAME, PASSWORD, "users");
-} else {
-    // 创建数据库
-    $sql = "CREATE DATABASE `users` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';";
-    if ($link->query($sql) === true) {
-        return "数据库创建成功";
-        $link = new mysqli(SERVERNAME, USERNAME, PASSWORD, "users");
-    } else {
-        return "数据库创建失败";
-    }
-}
-
-// 查询数据表是否存在
-if (mysqli_num_rows($link->query("SHOW TABLES LIKE 'pl_userslist'"))) {
-    return '该表存在';
-} else {
-    // 创建表
-    $sql = "CREATE TABLE `users`.`pl_userslist`  (
-                `iProsperId` int(11) NOT NULL AUTO_INCREMENT,
-                `name` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
-                `password` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
-                `phone` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '手机号',
-                `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '邮箱',
-                PRIMARY KEY (`iProsperId`)
-            );";
-    if ($link->query($sql) === true) {
-        return "新表插入成功";
-    } else {
-        return "新表插入失败";
-    }
 }
 
 // 输出json
